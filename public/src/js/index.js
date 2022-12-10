428abcbd3580c2c7a84e57f846e546b70d8684d6
KMS_Bismarck <exoduzen2524@gmail.com>
2022-12-10 11:11:45 +0800

    For review
    asd

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -1,9 +1,5 @@
 let toggleNav = document.getElementById('toggleNav');
 let pageNav = document.getElementById('pageNav');
 toggleNav.addEventListener('click', function() {
-    if (pageNav.style.display === "none") {
-        pageNav.style.display = "block";
-      } else {
-        pageNav.style.display = "none";
-      }
+   pageNav.classList.toggle('hide');
 })
